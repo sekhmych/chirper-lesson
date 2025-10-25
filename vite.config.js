38f3db39b1cbc0829368fcd6cb589ec2eba296a4
sekhmych <sekhmych@gmail.com>
2025-10-25 17:00:39 +0900

    test

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        input: ['resources/css/app.css', 'resources/js/app.js']
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
