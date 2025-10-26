31deb9448a9d6ba3811b357bbbc0fd2f1bde7658
sekhmych <sekhmych@gmail.com>
2025-10-26 16:06:48 +0900

    manifest

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     build: {
         manifest: true,
-        outDir: 'public/build',
+        outDir: '/public/build',
         input: ['resources/js/app.js']
     },
     plugins: [
